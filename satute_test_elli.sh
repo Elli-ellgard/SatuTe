82d7e41d2acd25d168c91a76f6218018341bb42d
Christiane Elgert <christiane.elgert@gmx.de>
2023-06-02 10:52:05 +0200

    test case only fasta file

--- satute_test_elli.sh
+++ satute_test_elli.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 
-set -e
+#set -e
 
 path=$(pwd)
 iqtree_path=/home/elgert/IQ-TREE/iqtree-2.2.2.4-Linux/bin/iqtree2
@@ -12,9 +12,21 @@ echo ""
 
 echo "-----------------------------------"
 echo "TEST 1: empty directory"
-cd $path/test/case_empty
-rm -r *
-python3 satute_cli.py  -iqtree $iqtree_path -dir $path/test/case_empty/
+echo""
+    DIR=$path/test/case_empty
+    if [ -d "$DIR" ]
+    then
+        if [ "$(ls -A $DIR)" ]; then
+             echo "Take action $DIR is not Empty"
+             rm -r $DIR/*
+        else
+            echo "$DIR is Empty"
+	fi
+    else
+        echo "Directory $DIR not found."
+    fi
+    
+    python3 satute_cli.py  -iqtree $iqtree_path -dir $DIR
 
 echo "-----------------------------------"
 echo ""
@@ -22,6 +34,13 @@ echo ""
 
 echo "-----------------------------------"
 echo "TEST 2a: only fasta alignment file"
+echo ""
+    DIR=$path/test/case_fasta
+    python3 satute_cli.py  -iqtree $iqtree_path -dir $DIR
+    mv example.fasta $path
+    rm -r $DIR
+    mkdir $DIR
+    mv $path/example.fasta  $DIR
 
 echo "-----------------------------------"
 echo ""
@@ -29,6 +48,7 @@ echo ""
 
 echo "-----------------------------------"
 echo "TEST 2b: only phylip alignment file"
+echo ""
 
 echo "-----------------------------------"
 echo ""
@@ -36,6 +56,8 @@ echo ""
 
 echo "-----------------------------------"
 echo  "TEST 3: directory with complete iqtree output"
+echo ""
+
 echo "-----------------------------------"
 echo ""
 
