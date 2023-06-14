37af535f43abddf3842aae5c0ca3bc3c849b1e3f
Christiane Elgert <christiane.elgert@gmx.de>
2023-06-14 16:38:47 +0200

    fix bugs: fasta, +R+G, -dir

--- satute_call_script.sh
+++ satute_call_script.sh
@@ -2,6 +2,6 @@ rm -r test/octo-kraken-msa-test/clades
 rm -r test/octo-kraken-msa-test/subsequences        
 python remove_files_except.py -m  
 # python satute_cli.py -dir /Users/berksakalli/Projects/Satute/test/octo-kraken-msa-test -iqtree iqtree -model GTR+F
-python satute_cli.py -tree ./test/octo-kraken-msa-test/example.phy.tree -msa ./test/octo-kraken-msa-test/example.phy -iqtree iqtree  -model GTR+G+G4
+python3 satute_cli.py -tree ./test/octo-kraken-msa-test/example.phy.tree -msa ./test/octo-kraken-msa-test/example.phy -iqtree iqtree  
 # rm -r test/octo-kraken-msa-test/clades  
 # rm -r test/octo-kraken-msa-test/subsequences        
--- satute_call_script_elli.sh
+++ satute_call_script_elli.sh
@@ -2,6 +2,8 @@ iqtree=/home/elgert/IQ-TREE/iqtree-2.2.2.4-Linux/bin/iqtree2
 rm -r test/octo-kraken-msa-test/clades  
 rm -r test/octo-kraken-msa-test/subsequences      
 python3 remove_files_except.py -m  
-# python satute_cli.py -dir /Users/berksakalli/Projects/Satute/test/octo-kraken-msa-test -iqtree iqtree -model GTR+F
-python3 satute_cli.py -iqtree $iqtree -tree ./test/octo-kraken-msa-test/example.phy.treefile  -msa ./test/octo-kraken-msa-test/example.phy -model GTR+G4 -alpha 0.05
+#python3 satute_cli.py -dir /home/elgert/Desktop/Cassius/octo-kraken -iqtree iqtree 
+python3 satute_cli.py -iqtree $iqtree -msa /home/elgert/Desktop/Cassius/octo-kraken/test/octo-kraken-msa-test/example.phy -model GTR+G -alpha 0.05
 # rm -r test/octo-kraken-msa-test/clades        
+#python3 satute_cli.py -iqtree $iqtree -dir /home/elgert/Desktop/Neue_Daten_2023_05_12/results/pbmc3k/subsampled_alignment_pbmc3k_100hvg_genewisenormed/satute/
+
