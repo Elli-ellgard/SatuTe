1f5eeb255f4576dddeabafbe3a0e94cf106b5812
Christiane Elgert <christiane.elgert@gmx.de>
2023-07-14 12:33:21 +0200

    split and clean up the code

---
+++ satute_rate_categories_and_alignments.py
@@ -0,0 +1,192 @@
+import os
+from Bio import AlignIO
+from Bio.Align import MultipleSeqAlignment
+import numpy as np
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+
+
+""" ## RATE CATEGORIES  """
+
+def get_column_names_with_prefix(data_frame, prefix):
+    # Filter the columns using the specified prefix
+    columns_with_prefix = data_frame.columns[
+        data_frame.columns.str.startswith(prefix)
+    ].tolist()
+    return columns_with_prefix
+
+def build_categories_by_sub_tables(data_frame):
+    rate_category_dictionary = {}
+
+    # Assuming you already have a dataframe called 'dataframe'
+    # Call the get_columns_with_prefix function to retrieve columns with a specific prefix
+    prefix = "p"  # Specify the desired prefix
+
+    columns_with_prefix = get_column_names_with_prefix(data_frame, prefix)
+
+    # Create dictionaries using column names with the specified prefix as names
+    rate_category_dictionary = {column: [] for column in columns_with_prefix}
+
+    for index, row in data_frame.iterrows():
+        p_row = row.filter(like="p")
+
+        rate_category_dictionary[p_row.idxmax()].append(int(row["Site"]) - 1)
+
+    return rate_category_dictionary
+
+def parse_category_rates(log_file):
+    f = open(log_file, "r")
+    lines = f.readlines()
+    f.close()
+    table_data = []
+
+    # Find the start and end indices of the table
+    start_index = -1
+    end_index = -1
+
+    for i, line in enumerate(lines):
+        if line.strip().startswith("Category"):
+            start_index = i + 1
+        elif line.strip().startswith(
+            "Relative rates are computed as MEAN of the portion of the Gamma distribution falling in the category."
+        ):
+            end_index = i
+            break
+
+    if start_index == -1 or end_index == -1:
+        raise ValueError("Table not found in the log file.")
+
+    # Parse the table rows
+    table_lines = lines[start_index:end_index]
+
+    for line in table_lines:
+        line = line.strip()
+        if line:
+            row = line.split()
+            category = row[0]
+            relative_rate = float(row[1])
+            proportion = float(row[2])
+            if category != "0":
+                table_data.append(
+                    {
+                        "Category": category,
+                        "Relative_rate": relative_rate,
+                        "Proportion": proportion,
+                    }
+                )
+    print(table_data)
+    return table_data
+
+""" ## HANDLE ALIGNMENTS  """
+
+def filter_alignment_by_ids(alignment, ids):
+    """
+    Filter a MultipleSeqAlignment object to include only sequences with specific IDs.
+
+    Parameters:
+    alignment (MultipleSeqAlignment): The alignment to filter.
+    ids (list of str): The IDs of the sequences to include.
+
+    Returns:
+    MultipleSeqAlignment: The filtered alignment.
+    """
+
+    # Filter the alignment
+    filtered_alignment = MultipleSeqAlignment(
+        [record for record in alignment if record.id in ids]
+    )
+
+    return filtered_alignment
+
+def guess_alignment_format(file_name):
+    with open(file_name, "r") as f:
+        first_line = f.readline().strip()
+
+    # Now we'll check for various signatures that might indicate the format.
+    if first_line.startswith(">"):
+        return "fasta"
+    elif first_line.startswith("CLUSTAL"):
+        return "clustal"
+    elif first_line.startswith("# STOCKHOLM"):
+        return "stockholm"
+    elif first_line.startswith("#NEXUS"):
+        return "nexus"
+    elif first_line.startswith("PileUp"):
+        return "pileup"
+    elif first_line[0].isdigit():
+        return "phylip"
+    else:
+        return "Unknown"
+
+def read_alignment_file(file_name):
+    # Guess the format of the file
+    file_format = guess_alignment_format(file_name)
+
+    # If the format could not be guessed, raise an error
+    if file_format is None:
+        raise ValueError("Could not guess the format of the file.")
+
+    # Try to read the file in the guessed format
+    try:
+        alignment = AlignIO.read(file_name, file_format)
+        return alignment
+    except Exception as e:
+        print(f"An error occurred while reading the file: {str(e)}")
+
+def cut_alignment_columns(alignment, columns):
+    # Convert the alignment to a NumPy array for easy column slicing
+    alignment_array = np.array([list(rec) for rec in alignment], np.character)
+    # Select the specified columns
+
+    selected_columns = alignment_array[:, columns]
+    selected_records = []
+    for i, rec in enumerate(selected_columns):
+        selected_records.append(
+            SeqRecord(Seq(rec.tobytes().decode()), id=alignment[i].id)
+        )
+    selected_alignment = MultipleSeqAlignment(selected_records)
+
+    return selected_alignment
+
+def write_alignment(alignment, file_name, file_format):
+    """
+    Write a MultipleSeqAlignment object to a file.
+
+    Parameters:
+    alignment (MultipleSeqAlignment): The alignment to write.
+    file_name (str): The name of the file to write to.
+    file_format (str): The format of the alignment file. This must be a string specifying one
+                       of the formats that Biopython supports, such as "fasta", "clustal", "phylip", etc.
+
+    Returns:
+    int: The number of alignments written (should be 1 for a MultipleSeqAlignment object).
+    """
+
+    # Write the alignment to the file
+    num_alignments_written = AlignIO.write(alignment, file_name, file_format)
+
+    return num_alignments_written
+
+def split_msa_into_rate_categories(site_probability, folder_path, msa_file_name):
+    sub_category = build_categories_by_sub_tables(site_probability)
+    alignment = read_alignment_file(msa_file_name)
+    per_category_alignment_dict = {}
+    valid_category_rates = []
+
+    for key, value in sub_category.items():
+        if len(value) > 0:
+            valid_category_rates = [int(key[1:])] + valid_category_rates
+        per_category_alignment_dict[key] = cut_alignment_columns(alignment, value)
+
+
+    for key, value in per_category_alignment_dict.items():
+        # Make a new directory for this subsequence, if it doesn't exist yet
+        os.makedirs(f"./{folder_path}/subsequence{key[1:]}/", exist_ok=True)
+
+        write_alignment(
+            value, f"./{folder_path}/subsequence{key[1:]}/rate.fasta", "fasta"
+        )
+
+    return valid_category_rates
+
+
---
+++ satute_trees_and_subtrees.py
@@ -0,0 +1,124 @@
+from ete3 import Tree
+import pandas as pd
+
+
+def parse_file_to_data_frame(file_path):
+    try:
+        # Read the file into a dataframe
+        df = pd.read_csv(file_path, delimiter="\t")
+
+        return df
+
+    except FileNotFoundError:
+        raise Exception(f"File not found: {file_path}")
+
+def parse_newick_file(file_path):
+    try:
+        # Open the file in read mode
+        with open(file_path, "r") as f:
+            # Read the content of the file
+            newick_string = f.readlines()
+
+        # Parse the newick string into a Tree object
+        t = Tree(newick_string[0], format=1)
+
+        # Return the parsed Tree object
+        return t
+
+    except FileNotFoundError:
+        raise Exception("File not found: " + file_path)
+
+def get_leaves(tree):
+    leaves = []
+    for leaf in tree:
+        if leaf.is_leaf():
+            leaves.append(leaf.name)
+    return leaves
+
+def branch_lengths(T):
+    vector_branches = []
+    vector_distances = []
+    internal_nodes = []
+
+    for node in T.traverse("levelorder"):
+        if (node not in T.get_leaves()) and (node in T.get_tree_root()):
+            internal_nodes.append(node.name)
+
+    for node in T.traverse("levelorder"):  # First internal branches.
+        children = node.get_children()
+
+        for child in children:
+            if child.name in internal_nodes:
+                vector_branches.append(node.name + "-" + child.name)
+                vector_distances.append(T.get_distance(node, child))
+
+    for node in T.traverse("levelorder"):  # Same for external branches.
+        children = node.get_children()
+
+        for child in children:
+            if child.name not in internal_nodes:
+                vector_branches.append(node.name + "-" + child.name)
+                vector_distances.append(T.get_distance(node, child))
+
+    return vector_branches, vector_distances
+
+def name_nodes_by_level_order(tree):
+    i = 1
+    for node in tree.traverse("levelorder"):
+        if not node.is_leaf():
+            node.name = f"Node{i}*"
+            i += 1
+    return tree
+
+def rescale_branch_lengths(tree, rescale_factor):
+    """
+    Rescales the branch lengths of a phylogenetic tree by a given factor.
+
+    Args:
+        tree (ete3.Tree): The input phylogenetic tree object.
+        rescale_factor (float): Scaling factor for the branch lengths.
+
+    Returns:
+        ete3.Tree: The phylogenetic tree object with rescaled branch lengths.
+    """
+    # Iterate over tree nodes and rescale branch lengths
+    for node in tree.traverse():
+        if node.up:
+            node.dist *= rescale_factor
+
+    # Return the tree with rescaled branch lengths
+    return tree
+
+def get_all_subtrees(tree):
+    subtrees = []
+    for node in tree.traverse("levelorder"):
+        if not node.is_root():
+            subtrees.append(node)
+    return subtrees
+
+def get_opposite_subtree(tree, subtree):
+    # Create a copy of the tree so as not to modify the original
+    tree_copy = Tree(tree.write())
+    tree_copy = name_nodes_by_level_order(tree_copy)
+    # Get the node in the copied tree that corresponds to the root of the subtree
+    node = tree_copy.search_nodes(name=subtree.name)[0]
+    # Detach this node (and its descendants) from the tree
+    node.detach()
+    return tree_copy
+
+def generate_subtree_pair(subtrees, t):
+    subtree_pairs = []
+    for subtree in subtrees:
+        subtree_copy = t.search_nodes(name=subtree.name)[0]
+        opposite_subtree = get_opposite_subtree(t, subtree_copy)
+
+        subtree_pair_entry = {
+            "trees": (subtree_copy, opposite_subtree),
+        }
+
+        subtree_pairs.append(subtree_pair_entry)
+
+    return subtree_pairs
+
+
+
