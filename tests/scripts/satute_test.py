f9672643b6b014a5f9d039a11622864024a9e7b1
Christiane Elgert <christiane.elgert@gmx.de>
2024-01-31 16:39:14 +0100

    test for satute option -msa

--- tests/scripts/satute_test.py
+++ tests/scripts/satute_test.py
@@ -5,20 +5,108 @@ import glob
 from pathlib import Path
 
 
-def print_info(suffix):
-    print(" ============= MODI MSA ====================")
+def print_test_name(suffix):
     print("")
+    print("---------------------------------------------------------")
     print(suffix)
     print("---------------------------------------------------------")
 
+def print_colored_message(message, color_code):
+    print(f"\033[{color_code}m{message}\033[0m")
 
-def create_dir_copy_with_suffix(source_dir, suffix) -> Path:
-    suffix_dir = source_dir + "_" + suffix.replace(" ", "_").replace(":", "")
-    if os.path.exists(suffix_dir):
-        shutil.rmtree(suffix_dir)
-    os.makedirs(suffix_dir, exist_ok=True)
-    shutil.copytree(source_dir, suffix_dir, dirs_exist_ok=True)
-    return suffix_dir
+def create_destination_dir(source_dir, suffix, base_dir="../test_results/") -> Path:
+    source_path = Path(source_dir)
+    base_path = Path(base_dir)
+    
+    # Check if the source directory exists
+    if not source_path.is_dir():
+        raise ValueError("Source directory does not exist.")
+    
+    # Create a suffix for the new directory name
+    suffix_cleaned = suffix.replace(" ", "_").replace(":", "")
+    
+    # Create the destination directory path
+    dest_dir = base_path / (source_path.name + "_" + suffix_cleaned)
+    
+    # Remove destination directory if it already exists
+    if dest_dir.is_dir():
+        shutil.rmtree(dest_dir)
+    
+    # Create the destination directory
+    dest_dir.mkdir(parents=True, exist_ok=True)
+    
+    return dest_dir
+
+def copy_files_to_dest_dir(source_dir, dest_dir, files_to_copy):
+    source_path = Path(source_dir)
+    dest_path = Path(dest_dir)
+    
+    # Check if both source and destination directories exist
+    if not source_path.is_dir() or not dest_path.is_dir():
+        raise ValueError("Source or destination directory does not exist.")
+    
+    # Copy only the specified files from source to destination
+    for file_name in files_to_copy:
+        source_file_path = source_path / file_name
+        dest_file_path = dest_path / file_name
+        
+        # Check if the file exists in the source directory
+        if source_file_path.is_file():
+            shutil.copy2(source_file_path, dest_file_path)
+        else:
+            print(f"Warning: File '{file_name}' not found in the source directory.")
+
+
+def check_files_exist(directory, file_list, name):
+    directory_path = Path(directory)
+    
+    # Check if the directory exists
+    if not directory_path.is_dir():
+        raise ValueError(f"Directory '{directory}' does not exist.")
+    
+    missing_files = [file for file in file_list if not (directory_path / file).is_file()]
+    
+    if missing_files:
+        print(f"The following {name} files are missing in the directory '{directory}': {', '.join(missing_files)}")
+        return False
+    else:
+        print(f"All {name} files are present in the directory '{directory}'.")
+        return True
+
+
+def additional_iqtree_files(options):
+    suffix = []
+    if "ufboot" in options:
+        suffix.extend([".contree", ".splits.nex"])
+    elif "boot" in options:
+        suffix.extend([".boottrees", ".contree"])
+    elif "wspr" in options: 
+        suffix.append(".siteprob")
+    return suffix
+
+def check_iqtree_files_exist(data_name, dest_dir_path, iqtree_options):
+    iqtree_files_endings = [".bionj",".ckp.gz",".iqtree",".log",  ".mldist", ".model.gz", ".treefile"]
+    if len(iqtree_options):
+        iqtree_files_endings.extend(additional_iqtree_files(iqtree_options))
+    files_to_check = [ data_name + suffix for suffix in iqtree_files_endings]
+    return check_files_exist(dest_dir_path, files_to_check, "IQ-TREE")
+
+
+def check_satute_files(data_name, dest_dir_path, categories, alpha, asr):
+    file_endings = [f"_{alpha}.satute.log"]
+    suffix = [".satute.csv",".satute.nex"]
+    if asr: 
+        suffix.append(".satute.asr.csv")
+    if len(categories):
+        for rate in categories:
+            category_endings = [f"_p{rate}_{alpha}{x}" for x in suffix]
+            file_endings.extend(category_endings)
+    else:
+        file_endings.extend([f"_single_rate_{alpha}{x}" for x in suffix])
+        
+    files_to_check = [ data_name + suffix for suffix in file_endings]  
+    return check_files_exist(dest_dir_path, files_to_check, "Satute")
+
 
 
 def run_external_command(command_args):
@@ -518,10 +606,16 @@ def test_10c_valid_category(dir_path, msa, iqtree):
 
 
 def main():
-    dir_path = "./test/Clemens/toy_example_JC"
-    msa = "toy_example_ntaxa_7_run_5-alignment.phy"
+
+    # set paths to IQ-TREE and Python executable
     path_iqtree = "iqtree2"
     path_python = "python3"
+
+
+
+    dir_path = "./test/Clemens/toy_example_JC"
+    msa = "toy_example_ntaxa_7_run_5-alignment.phy"
+
 
     test_1a(dir_path, msa, path_iqtree, path_python)
     test_1b(dir_path, msa, path_iqtree, path_python)
---
+++ tests/scripts/test_option_msa.py
@@ -0,0 +1,159 @@
+from satute_test import (
+    run_external_command,
+    print_test_name, 
+    create_destination_dir,
+    copy_files_to_dest_dir,
+    check_iqtree_files_exist,
+    check_satute_files,
+    print_colored_message
+    
+)
+import os
+import shutil
+import subprocess
+import glob
+from pathlib import Path
+
+
+
+def test_1a(source_path, msa, iqtree, python, satute):
+    suffix = "TEST 1a: only msa iqtree exists"
+    print_test_name(suffix)
+
+    # create output directory
+    dest_dir_path = create_destination_dir(source_path, suffix)
+
+    # copy msa file to output directory
+    files_to_copy = [msa]
+    copy_files_to_dest_dir(source_path, dest_dir_path, files_to_copy)
+
+    categories = []
+    alpha = str(0.05)
+    asr = True
+
+    # Satute run
+    run_external_command(
+        [
+            python,
+            satute,
+            "-iqtree",
+            iqtree,
+            "-msa",
+            os.path.join(dest_dir_path, msa),
+            "-alpha",
+            alpha,
+            "-asr",
+        ]
+   )
+
+    # check the files   
+    if check_iqtree_files_exist(msa, dest_dir_path, [] )  and check_satute_files(msa, dest_dir_path, categories, alpha, asr):
+        print_colored_message("TEST 1a was successful", "32" )
+    else: 
+        print_colored_message("TEST 1a failed", "31" )
+
+
+def test_1b(source_path, msa, iqtree, python, satute):
+    suffix = "TEST 1b: only msa ufboot"
+    print_test_name(suffix)
+
+    # create output directory
+    dest_dir_path = create_destination_dir(source_path, suffix)
+
+    # copy msa file to output directory
+    files_to_copy = [msa]
+    copy_files_to_dest_dir(source_path, dest_dir_path, files_to_copy)
+
+    categories = []
+    alpha = str(0.01)
+    asr = False
+
+    # Satute run
+    run_external_command(
+        [
+            python,
+            satute,
+            "-iqtree",
+            iqtree,
+            "-msa",
+            os.path.join(dest_dir_path, msa),
+            "-ufboot",
+            "1000",
+            "-alpha",
+            alpha,
+        ]
+    )
+
+    
+    # check the files   
+    if check_iqtree_files_exist(msa, dest_dir_path, ["ufboot"]) and check_satute_files(msa, dest_dir_path, categories, alpha, asr):
+        print_colored_message("TEST 1b was successful", "32" )
+    else: 
+        print_colored_message("TEST 1b failed", "31" )
+
+
+def test_1c(source_path, msa, iqtree, python, satute):
+    suffix = "TEST 1c: only msa boot"
+    print_test_name(suffix)
+
+    # create output directory
+    dest_dir_path = create_destination_dir(source_path, suffix)
+
+    # copy msa file to output directory
+    files_to_copy = [msa]
+    copy_files_to_dest_dir(source_path, dest_dir_path, files_to_copy)
+
+    categories = []
+    alpha = str(2.0)
+    asr = True
+
+
+    run_external_command(
+        [
+            python,
+            satute,
+            "-iqtree",
+            iqtree,
+            "-msa",
+            os.path.join(dest_dir_path, msa),
+            "-boot",
+            "100",
+            "-alpha",
+            alpha,
+            "-asr",
+        ]
+    )
+
+    # check the files   
+    if check_iqtree_files_exist(msa, dest_dir_path, ["boot"])  and    check_satute_files(msa, dest_dir_path, categories, alpha, asr):
+        print_colored_message("TEST 1c was successful", "32" )
+    else: 
+        print_colored_message("TEST 1d failed", "31" )
+
+
+def test_option_msa(path_iqtree, path_python, path_satute, source_path, msa, results_path):
+
+    print("")
+    print(" ============= MODI MSA ====================")
+    print("")
+
+
+    test_1a(source_path, msa, path_iqtree, path_python, path_satute)
+    test_1b(source_path, msa, path_iqtree, path_python, path_satute)
+    test_1c(source_path, msa, path_iqtree, path_python, path_satute)
+
+
+if __name__ == "__main__":
+    # set paths to IQ-TREE and Python executable
+    path_iqtree = "iqtree2"
+    path_python = "python3"
+    path_satute = "../../satute_cli.py"
+
+    # smallest toy example
+    data_dir_path = "../data/data_dna/toy_example_JC"
+    msa = "toy_example_ntaxa_7_run_5-alignment.phy"
+
+    output_dir_path =  "../test_results/"
+
+    test_option_msa(path_iqtree, path_python, path_satute, data_dir_path, msa, output_dir_path)
+    
