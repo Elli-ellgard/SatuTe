e1e54a77c4ec5231bf9b62f917bf6f9409604b01
EnesSakalliUniWien <enes.sakalli@univie.ac.at>
2024-05-14 11:34:53 +0200

    Issue #41 When taxon is not in the tree and the multiple sequence alignment, test added

--- tests/unittests/test_satute_sequences.py
+++ tests/unittests/test_satute_sequences.py
@@ -1,11 +1,12 @@
+import sys
+
+sys.path.append("./../..")
+
 import unittest
 from Bio.Align import MultipleSeqAlignment
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from ete3 import Tree
-import sys
-
-sys.path.append("./../..")
 from satute_sequences import check_if_alignment_has_same_taxa_as_msa, TaxaMismatchError
 
 
@@ -36,6 +37,11 @@ class TestCheckIfAlignmentHasSameTaxaAsMSA(unittest.TestCase):
         self.tree_non_matching.add_child(name="taxon1")
         self.tree_non_matching.add_child(name="taxon2")
         self.tree_non_matching.add_child(name="taxon4")  # Different taxon
+        
+        # Create a Tree object with partially matching taxa
+        self.tree_partial_matching = Tree(name="root")
+        self.tree_partial_matching.add_child(name="taxon1")
+        self.tree_partial_matching.add_child(name="taxon2")        
 
     def test_matching_taxa(self):
         try:
@@ -55,11 +61,22 @@ class TestCheckIfAlignmentHasSameTaxaAsMSA(unittest.TestCase):
             "The taxa sets in the alignment and the tree do not match.",
             exception_message,
         )
+
         self.assertIn(
             "Taxa missing in tree: taxon3.", exception_message
         )  # Missing in tree
+
         self.assertIn("Extra taxa in tree: taxon4.", exception_message)  # Extra in tree
 
+    def test_partial_matching_taxa(self):
+        with self.assertRaises(TaxaMismatchError) as cm:
+            check_if_alignment_has_same_taxa_as_msa(self.alignment, self.tree_partial_matching)
+                    
+        # Check specific parts of the error message
+        exception_message = str(cm.exception)
+        self.assertIn("The taxa sets in the alignment and the tree do not match.", exception_message)
+        self.assertIn("Taxa missing in tree: taxon3.", exception_message)  # Missing in tree
+        self.assertNotIn("Extra taxa in tree:", exception_message)  # No extra taxa
 
 if __name__ == "__main__":
     unittest.main()
